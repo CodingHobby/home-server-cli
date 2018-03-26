866277dd3777064f9ab4997c369724488836e1aa
Corrado Canepari <codinghobby@gmail.com>
2018-03-26 21:15:39 +0200

    Updated to latest homeServer version

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ const prog = require('caporal'),
 function reverseLookup(ip, callback) {
 	let domain
 	dns.reverse(ip, function (err, domains) {
-		callback(domains[0])
+		callback(domains ? domains[0] : undefined)
 	})
 }
 
@@ -18,22 +18,13 @@ prog
 	.command('start', 'Start a web server')
 	.argument('[path]', 'Shared folder relative path', null, '.')
 	.action(function (args, options, logger) {
-		let homeServer = new server(args.path)
+		let homeServer = new server.homeServer(args.path)
 		homeServer.start()
 	})
 	.command('scan', 'Find all running web servers on network')
 	.action(function (args, options, logger) {
 		logger.info(colors.green('Found following devices:'))
-		for (let ip = 1; ip < 254; ip++) {
-			let host = `${address().split('.').slice(0, 3).join('.')}.${ip}`
-			let s = new net.Socket()
-			s.setTimeout(2000, () => s.destroy())
-			s.connect(8080, host, () => reverseLookup(host, d => logger.info(`${colors.green(d)}: http://${host}:8080`)))
-			s.on('error', function (e) {
-				s.destroy()
-			})
-		}
+		server.utils.scan(host => server.utils.reverseLookup(host, d => logger.info(`${colors.green(d)}: http://${host}:8080`)))
 	})
-
 
 prog.parse(process.argv)
